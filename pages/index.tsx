bbe395333097916d9d2f1c68a2179608fd9958f0
Arydiane <arydianejardim@gmail.com>
2023-11-17 18:16:19 -0300

    atualiza página adiciona provedor de tema

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,17 @@
 import Button from "../src/components/Button/Button";
 import Text from "../src/components/Text/Text";
+import ThemeProvider from "../src/theme/ThemeProvider";
 
 export default function HomeScreen() {
   return (
-    <div>
-      <h1>Home Page!</h1>
-      <Text tag='h2'>Esse é um exemplo de texto usando a tag para titulo h2</Text>
-      <Button>
-        Botão Simples
-      </Button>
-
-    </div>
+    <ThemeProvider>
+      <div>
+        <h1>Home Page!</h1>
+        <Text tag="h2">
+          Esse é um exemplo de texto usando a tag para titulo h2
+        </Text>
+        <Button variant="accent">Botão Simples</Button>
+      </div>
+    </ThemeProvider>
   );
 }
